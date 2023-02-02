daa9b8c2d33fa633330e01386fc5f6a67231366f
Augustin <augustin.ch.he@gmail.com>
2023-02-02 21:09:49 +0100

    set 3.7v of flutter

--- android/app/src/main/kotlin/com/example/sbc_drink_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sbc_drink_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ sbcdrink_app/android/app/src/main/kotlin/com/example/sbcdrink_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sbcdrink_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
