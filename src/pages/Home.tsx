8d7c13a6cb72bb54c598e598ffa110a73a663380
loic.huart <loic.huart08@gmail.com>
2024-04-22 20:24:48 +0200

    fix: background scroll list cocktail

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,23 +39,31 @@ const Home: FC = () => {
   }
 
   return (
-    <Container sx={{ paddingTop: '15px' }}>
-      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-              <TextNeon text="SBCDRINK" type={2} style={{ fontSize: 40 }}/>
-          </Typography>
-          <SwitchTheme sx={{ m: 1 }} checked={theme.palette.mode === 'dark'} onChange={(event) => {
-            setMode(event.target.checked ? 'dark' : 'light')
-          }} />
-      </Stack>
+    <div
+      style={{
+        backgroundImage: 'url("/assets/images/beach_summer_2.png")',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}
+    >
+       <Container sx={{ paddingTop: '15px' }}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+            <Typography variant="h4" gutterBottom>
+                <TextNeon text="SBCDRINK" type={2} style={{ fontSize: 40 }}/>
+            </Typography>
+            <SwitchTheme sx={{ m: 1 }} checked={theme.palette.mode === 'dark'} onChange={(event) => {
+              setMode(event.target.checked ? 'dark' : 'light')
+            }} />
+        </Stack>
 
-      <ListCocktail cocktails={listCocktails} />
-      <Stack direction="row" alignItems="center" justifyContent="flex-end" mb={5} mt={5}>
-          <Button variant="contained" onClick={() => navigate(paths.dashboard.bottle)} sx={{ mb: 5 }}>
-              Manage
-          </Button>
-      </Stack>
-    </Container>
+        <ListCocktail cocktails={listCocktails} />
+        <Stack direction="row" alignItems="center" justifyContent="flex-end" mb={5} mt={5}>
+            <Button variant="contained" onClick={() => navigate(paths.dashboard.bottle)} sx={{ mb: 5 }}>
+                Manage
+            </Button>
+        </Stack>
+      </Container>
+    </div>
   )
 }
 
